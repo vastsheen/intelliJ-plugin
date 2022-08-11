9ab5512a43087559cc497af23677796642b7ae92
GitHub Action <action@github.com>
2022-08-11 03:32:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/vastsheen/intellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/vastsheen/intellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.vastsheen.intellijplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.vastsheen.intellijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/vastsheen/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vastsheen/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vastsheen.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vastsheen.intellijplugin.MyBundle
 
 class MyApplicationService {
 
